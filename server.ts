73276b77b58b9e450057cad000af545685b20e22
nika <nika3141535@gmail.com>
2025-04-12 02:05:57 +0400

    add prisma connection

--- server.ts
+++ server.ts
@@ -1,5 +1,14 @@
 import express from "express"
+import moragn from "morgan"
+import * as dotenv from "dotenv"
 
 const app = express()
+
+if(process.env.NODE_ENV == "development"){
+    app.use(moragn("dev"))
+}
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 
 export default app
